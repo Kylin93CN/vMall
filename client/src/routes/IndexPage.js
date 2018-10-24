6bec7b70b6e624c84afd1680d6c78b52a77cccad
Kylin93CN <375020612@qq.com>
2018-10-24 17:10:24 +0800

    a commit

--- client/src/routes/IndexPage.js
+++ client/src/routes/IndexPage.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { connect } from 'dva';
 import { Row, Col } from 'antd';
+import Header from '../components/Header';
 import styles from './IndexPage.css';
 
-const IndexPage = ({children}) => {
+const IndexPage = ({ children }) => {
   return (
     <div className={styles.normal}>
       <Row className={styles.header}>
-        <Col span={24}>header</Col>
+        <Col span={24} style={{ height: '100%' }}>
+          <Header />
+        </Col>
       </Row>
       <Row className={styles.body}>
         <Col className={styles.sider} span={6}>sider</Col>
@@ -17,7 +20,7 @@ const IndexPage = ({children}) => {
       </Row>
     </div>
   );
-}
+};
 
 IndexPage.propTypes = {
 };
