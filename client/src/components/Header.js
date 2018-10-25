e623a6058a20c23552001d964d53d05ff75e8c0b
Kylin93CN <375020612@qq.com>
2018-10-25 11:27:24 +0800

    a commit

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,50 +1,44 @@
 import React from 'react';
 import { Icon } from 'antd';
-import styles from './Header.css';
+import styles from './Header.scss';
 
 const Header = () => {
   return (
-	<div>
-		<ul style={{ float: 'left',marginTop: '5px',listSytle: 'none' }}>
-			<li>
-				<Icon type="star" theme="filled" spin={false} style={{ color: "#cccccc" }} />
-				<span>收藏本站</span>
-			</li>
-			<li>
-				<Icon type="environment" theme="filled" style={{ color: "#cccccc" }} />
-				<span>西湖区</span><span>[更换]</span>
-			</li>
-		</ul>
-		<ul  style={{ float: 'right',marginTop: '5px' }}>
-			<li>
-				<span>[登录]</span>
-			</li>
-			<li>
-				<span>[登录]</span>
-			</li>
-			<li>
-				<span>[登录]</span>
-			</li>
-			<li>
-				<span>[注册]</span>
-			</li>
-			<li>
-				<span className={styles.headerMenu}>我的订单</span>
-			</li>
-			<li>
-				<span className={styles.headerMenu}>我的家乐福</span>
-			</li>
-			<li>
-				<span className={styles.headerMenu}>移动端</span>
-			</li>
-			<li>
-				<span className={styles.headerMenu}>客户服务</span>
-			</li>
-			<li>
-				<span className={styles.headerMenu}>EnglishSite</span>
-			</li>
-		</ul>
-	</div>    
+		<div>
+			<ul className={styles.headerLeft}>
+				<li>
+					<Icon type="star" theme="filled" spin={false} style={{ color: "#cccccc", fontSize: '12px' }} />
+					<span>收藏本站</span>
+				</li>
+				<li>
+					<Icon type="environment" theme="filled" style={{ color: "#cccccc", fontSize: '12px' }} />
+					<span>西湖区</span><span>[更换]</span>
+				</li>
+			</ul>
+			<ul className={styles.headerRight}>
+				<li>
+					<span>[登录]</span>
+				</li>
+				<li>
+					<span>[注册]</span>
+				</li>
+				<li>
+					<span className={styles.headerMenu}>我的订单</span>
+				</li>
+				<li>
+					<span className={styles.headerMenu}>我的家乐福</span>
+				</li>
+				<li>
+					<span className={styles.headerMenu}>移动端</span>
+				</li>
+				<li>
+					<span className={styles.headerMenu}>客户服务</span>
+				</li>
+				<li>
+					<span className={styles.headerMenu}>EnglishSite</span>
+				</li>
+			</ul>
+		</div>
   );
 };
 
