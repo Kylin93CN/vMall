0047d42fa4f2ec747d170fe6804e37f32ff2a6f8
Kylin93CN <375020612@qq.com>
2018-10-25 16:52:35 +0800

    a commit

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,16 +3,35 @@ import { Icon } from 'antd';
 import styles from './Header.scss';
 
 const Header = () => {
+	const _addFavorite = () => {
+		const url = window.location;
+		const title = '家乐福超市';
+		try
+			{
+					window.external.addFavorite(url, title);
+			}
+			catch (e)
+			{
+					try
+					{
+							window.sidebar.addPanel(url, title, "");
+					}
+					catch (e)
+					{
+							alert("加入收藏失败，请使用Ctrl+D进行添加");
+					}
+			}
+	}
   return (
 		<div>
 			<ul className={styles.headerLeft}>
 				<li>
 					<Icon type="star" theme="filled" spin={false} style={{ color: "#cccccc", fontSize: '12px' }} />
-					<span>收藏本站</span>
+					<span onClick={_addFavorite}>&nbsp;收藏本站</span>
 				</li>
 				<li>
 					<Icon type="environment" theme="filled" style={{ color: "#cccccc", fontSize: '12px' }} />
-					<span>西湖区</span><span>[更换]</span>
+					<span>&nbsp;西湖区&nbsp;</span><span>[更换]</span>
 				</li>
 			</ul>
 			<ul className={styles.headerRight}>
@@ -23,19 +42,25 @@ const Header = () => {
 					<span>[注册]</span>
 				</li>
 				<li>
-					<span className={styles.headerMenu}>我的订单</span>
+					<ins className={styles.verticalLine}/>
+					<span>我的订单</span>
+					<ins className={styles.downChoice}/>
 				</li>
 				<li>
-					<span className={styles.headerMenu}>我的家乐福</span>
+					<ins className={styles.verticalLine}/>
+					<span>我的家乐福</span>
 				</li>
 				<li>
-					<span className={styles.headerMenu}>移动端</span>
+					<ins className={styles.verticalLine}/>
+					<span>移动端</span>
 				</li>
 				<li>
-					<span className={styles.headerMenu}>客户服务</span>
+					<ins className={styles.verticalLine}/>
+					<span>客户服务</span>
 				</li>
 				<li>
-					<span className={styles.headerMenu}>EnglishSite</span>
+					<ins className={styles.verticalLine}/>
+					<span>EnglishSite</span>
 				</li>
 			</ul>
 		</div>
